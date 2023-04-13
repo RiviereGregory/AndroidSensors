57a9c004799dc66a7449791fc333f3899970fcc2
RiviereGregory <gregory.riviere@laposte.net>
2023-04-13 21:17:07 +0200

    feat: INIT - THE ENDOR MAP Projet

---
+++ TheEndorMap/app/src/main/java/gri/riverjach/theendormap/MainActivity.kt
@@ -0,0 +1,11 @@
+package gri.riverjach.theendormap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
