b699b9bcaf1878e4bc38eac396831036f6f2e678
RiviereGregory <gregory.riviere@laposte.net>
2023-04-20 20:05:44 +0200

    feat: THE ENDOR MAP - Ajout Permission location

--- TheEndorMap/app/src/main/java/gri/riverjach/theendormap/MainActivity.kt
+++ TheEndorMap/app/src/main/java/gri/riverjach/theendormap/MainActivity.kt
@@ -1,14 +1,59 @@
 package gri.riverjach.theendormap
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import timber.log.Timber
+
+private const val REQUEST_PERMISSION_LOCATION_LAST_LOCATION = 1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        updateLastLocation()
 
-        Timber.i("Hello from MainActivity")
+    }
+
+    private fun updateLastLocation() {
+        Timber.d("updateLocation")
+        if (!checkLocationPermission()) {
+            return
+        }
+
+        // CODE UPDATE LAST LOCATION
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_PERMISSION_LOCATION_LAST_LOCATION
+            )
+            return false
+        }
+        return true
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        when (requestCode) {
+            REQUEST_PERMISSION_LOCATION_LAST_LOCATION -> updateLastLocation()
+        }
     }
 }
