fa561ed3809e76d728cdf5e66f7dbe7395a26ea2
RiviereGregory <gregory.riviere@laposte.net>
2023-06-13 20:21:25 +0200

    feat: THE ENDOR MAP - initialisation des notifications à partir de Oréo

--- TheEndorMap/app/src/main/java/gri/riverjach/theendormap/App.kt
+++ TheEndorMap/app/src/main/java/gri/riverjach/theendormap/App.kt
@@ -1,13 +1,39 @@
 package gri.riverjach.theendormap
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import timber.log.Timber
 
 class App : Application() {
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID = "EndoMap"
+    }
 
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        }
+
+        val name = "Endor Map Notifications"
+        val descriptionText = "Be notified when you enter Middle Earth special areas"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
