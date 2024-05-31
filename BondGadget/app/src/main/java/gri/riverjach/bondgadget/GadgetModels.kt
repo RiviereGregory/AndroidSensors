2c0225f191f0dfc723b901a5484b937d9c15eff9
RiviereGregory <gregory.riviere@laposte.net>
2024-05-31 11:19:28 +0200

    feat: BOND GADGET - Ajout Parcelable sur class metier

--- BondGadget/app/src/main/java/gri/riverjach/bondgadget/GadgetModels.kt
+++ BondGadget/app/src/main/java/gri/riverjach/bondgadget/GadgetModels.kt
@@ -1,5 +1,7 @@
 package gri.riverjach.bondgadget
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.util.Date
 
 sealed class Gadget {
@@ -7,14 +9,16 @@ sealed class Gadget {
     abstract var dateCreated: Date
 }
 
+@Parcelize
 data class GadgetQRCode(
     override var id: Int = 0,
     override var dateCreated: Date = Date(),
     val url: String
-) : Gadget()
+) : Gadget(), Parcelable
 
+@Parcelize
 data class GadgetNFC(
     override var id: Int = 0,
     override var dateCreated: Date = Date(),
     val url: String
-) : Gadget()
+) : Gadget(), Parcelable
