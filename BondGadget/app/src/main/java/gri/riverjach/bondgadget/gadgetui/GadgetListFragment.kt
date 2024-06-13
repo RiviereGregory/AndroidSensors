63300af994d44d6fc93be9375574309030212ef8
RiviereGregory <gregory.riviere@laposte.net>
2024-06-13 20:38:34 +0200

    feat: BOND GADGET - Créer le ViewModelFactory commun

--- BondGadget/app/src/main/java/gri/riverjach/bondgadget/gadgetui/GadgetListFragment.kt
+++ BondGadget/app/src/main/java/gri/riverjach/bondgadget/gadgetui/GadgetListFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import gri.riverjach.bondgadget.App
 import gri.riverjach.bondgadget.R
 
 class GadgetListFragment : Fragment() {
 
+    private lateinit var viewModel: GadgetListViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,4 +21,15 @@ class GadgetListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_gadget_list, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val factory = GadgetUiViewModelFactory(App.repo)
+        viewModel =
+            ViewModelProviders.of(requireActivity(), factory).get(GadgetListViewModel::class.java)
+        viewModel.getViewState().observe(viewLifecycleOwner, Observer { updateUi(it!!) })
+    }
+
+    private fun updateUi(state: GadgetListViewState) {
+
+    }
 }
---
+++ BondGadget/app/src/main/java/gri/riverjach/bondgadget/gadgetui/GadgetUiViewModelFactory.kt
@@ -0,0 +1,17 @@
+package gri.riverjach.bondgadget.gadgetui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import gri.riverjach.bondgadget.repository.Repo
+
+@Suppress("UNCHECKED_CAST")
+class GadgetUiViewModelFactory(private val repo: Repo) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(GadgetListViewModel::class.java) -> GadgetListViewModel(repo)
+
+            else -> throw IllegalAccessException("Unexpected model class $modelClass")
+        } as T
+    }
+}
