f403b34ec77f300a28ada05563553c4792f4775a
RiviereGregory <gregory.riviere@laposte.net>
2024-06-13 20:16:15 +0200

    feat: BOND GADGET - Utiliser un MediatorLiveData pour le ViewState

--- BondGadget/app/src/main/java/gri/riverjach/bondgadget/gadgetui/GadgetListViewModel.kt
+++ BondGadget/app/src/main/java/gri/riverjach/bondgadget/gadgetui/GadgetListViewModel.kt
@@ -13,9 +13,26 @@ data class GadgetListViewState(
 
 class GadgetListViewModel(private val repo: Repo) : ViewModel() {
 
+    // MediatorLiveData permet d'appelle des liveData en cascade
     private val viewState = MediatorLiveData<GadgetListViewState>()
 
     fun getViewState(): LiveData<GadgetListViewState> = viewState
+
+    init {
+        viewState.addSource(repo.getAllGadgets()) { gadgets ->
+            val oldState = viewState.value!!
+            viewState.value = oldState.copy(
+                hasGadgetsChanged = true,
+                gadgets = gadgets
+            )
+        }
+
+        //initial state
+        viewState.value = GadgetListViewState(
+            hasGadgetsChanged = false,
+            gadgets = emptyList()
+        )
+    }
 
     fun addGadget(gadget: Gadget) {
         repo.addGadget(gadget)
