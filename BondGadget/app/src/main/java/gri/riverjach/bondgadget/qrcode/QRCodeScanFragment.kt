8b7e32833f65d98efd42f1fdf1fa18f9325fec25
RiviereGregory <gregory.riviere@laposte.net>
2024-10-16 18:48:18 +0200

    feat: BOND GADGET - Ajout autorisation CAMERA OLD version

--- BondGadget/app/src/main/java/gri/riverjach/bondgadget/qrcode/QRCodeScanFragment.kt
+++ BondGadget/app/src/main/java/gri/riverjach/bondgadget/qrcode/QRCodeScanFragment.kt
@@ -1,11 +1,17 @@
 package gri.riverjach.bondgadget.qrcode
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import gri.riverjach.bondgadget.R
+
+private const val PERMISSION_REQUEST_CAMERA = 1
 
 class QRCodeScanFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +20,36 @@ class QRCodeScanFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_qrcode_scan, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!hasCameraPermission()) {
+            requestCameraPermission()
+        }
+    }
+
+    private fun hasCameraPermission() =
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestCameraPermission() {
+        requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CAMERA -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                findNavController().popBackStack()
+            }
+        }
     }
 }
